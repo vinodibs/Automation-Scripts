fae506d154728e93de570240517b3e68e89dd773
Vinod Kumar <vinod.kumar@impressico.com>
2017-10-18 08:44:51 -0700

    MY Second Commit

---
+++ ubuntu-script/remmina.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+#################################################
+# Written to be used on x64 & x86 bits Computers
+# Written by Vinod Kumar 
+#################################################
+# DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+
+# Installing & Downloading remmina for Ubuntu Version 
+sudo apt-add-repository ppa:remmina-ppa-team/remmina-next
+sudo apt-get update
+sudo apt-get install remmina remmina-plugin-rdp libfreerdp-plugins-standard
+echo "################################################################"
+echo "##########    INSTALLATION HAS BE DONE      ###########"
+echo "###########################################"
