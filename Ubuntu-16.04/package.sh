3babf2a5ea7c6d0dc425e25844dc3f4bd23a5a56
Vinod Kumar <vinod.kumar@impressico.com>
2017-10-26 04:01:50 -0400

    Add package script file

---
+++ Ubuntu-16.04/package.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+########################################################
+# Written to be used on 64 bits computers
+# Written By    :       Vinod Kumar
+########################################################
+########################################################
+########################################################
+
+# Installing Required Package for Ubuntu Version
+sudo apt-get update -y 
+sudo apt-get install net-tools -y 
+sudo apt-get install network-manager* -y
+sudo apt-get install vim -y
+sudo apt-get install 
+sudo echo "192.168.0.1       "icloud.impressico.local" " >> /etc/hosts
+sudo echo "192.168.0.11       "impressico.local" " >> /etc/hosts
+sudo echo "192.168.0.68       "hrms.impressico.local" " >> /etc/hosts
+echo "################################################################"
+echo "################ Installation Sucessfully ######################"
+echo "################################################################"
