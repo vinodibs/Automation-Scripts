e2126e127a07e10e449c381e0eb03e5b3ca41247
Vinod Kumar <vinod.kumar@impressico.com>
2017-11-06 06:28:31 -0500

    sh fixed

--- Ubuntu-16.04/package.sh
+++ Ubuntu-16.04/package.sh
@@ -11,6 +11,7 @@ sudo apt-get update -y
 sudo apt-get install net-tools -y 
 sudo apt-get install network-manager* -y
 sudo apt-get install vim -y
+sudo apt-get -f install
 sudo echo "192.168.0.1       "icloud.impressico.local" " >> /etc/hosts
 sudo echo "192.168.0.11       "impressico.local" " >> /etc/hosts
 sudo echo "192.168.0.68       "hrms.impressico.local" " >> /etc/hosts
