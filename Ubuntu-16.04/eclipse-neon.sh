09e26b2f1e1dccafd9272f8f0eeea095fc7a10b4
Vinod Kumar <vinod.kumar@impressico.com>
2017-11-11 03:48:12 -0500

    Elipse Neon Scripts Add

---
+++ Ubuntu-16.04/eclipse-neon.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+########################################################
+# Written to be used on 64 bits computers
+# Written By    :       Vinod Kumar
+########################################################
+########################################################
+########################################################
+
+# Downloading & Installing elipse for Ubuntu Version
+
+wget http://git.impressicocrm.com/vinod.kumar/Automation-Scritps/raw/master/eclipse-jee-neon-R-linux-gtk-x86_64.tar.gz -O ~/Desktop/ 
+
+echo "################################################################"
+echo "################ Installation Sucessfully ######################"
+echo "################################################################"
+
