b29c58ed9bb1e44f5a9592b0b0bfb6da0cecd892
Vinod Kumar <vinod.kumar@impressico.com>
2017-11-11 17:36:14 +0530

    intellij

---
+++ Ubuntu-16.04/intelliJ.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+########################################################
+# Written to be used on 64 bits computers
+# Written By    :       Vinod Kumar
+########################################################
+########################################################
+########################################################
+
+# Downloading & Installing elipse for Ubuntu Version
+
+wget wget http://git.impressicocrm.com/vinod.kumar/Software/raw/master/ideaIU-2017.2.5.tar.gz -O /tmp/ideaIU-2017.2.5.tar.gz
+cd /tmp 
+tar -xvf ideaIU-2017.2.5.tar.gz -C ~/Desktop/
+echo "################################################################"
+echo "################ Installation Sucessfully ######################"
+echo "################################################################"
+
