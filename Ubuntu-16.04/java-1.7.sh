8ffa5c9fe9a76ee53f07332cab2a155edf521a65
Vinod Kumar <vinod.kumar@impressico.com>
2017-11-13 06:54:40 -0500

    Java 1.7

---
+++ Ubuntu-16.04/java-1.7.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+###############################################
+# Written to be used on 64 bits computers
+# Written By    :       Vinod Kumar
+###############################################
+###############################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+###############################################
+
+sudo add-apt-repository ppa:openjdk-r/ppa -y  
+sudo apt-get update -y   
+sudo apt-get install openjdk-7-jdk -y
+echo "################################################################"
+echo "################ Installation Sucessfully ######################"
+echo "################################################################"  
