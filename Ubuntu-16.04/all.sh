052f0d76f6e40285cd39b7be04c6c40f7161d3b5
Vinod Kumar <vinod.kumar@impressico.com>
2017-11-13 05:41:14 -0500

    Maven & all Repo

---
+++ Ubuntu-16.04/all.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+########################################################
+# Written to be used on 64 bits computers
+# Written By    :       Vinod Kumar
+########################################################
+########################################################
+########################################################
+
+# Installing All required Package for Ubuntu Version
+./package.sh
+./adobereader.sh
+./beyond-compare.sh
+./sh chrome.sh
+./eclipse-neon.sh
+./java-install.sh
+./notepadqq.sh
+./openssh.sh
+./owncloud.sh
+./remmina.sh
+./shutter.sh
+./skype.sh
+./slack.sh
+./sublime.sh
+./vlc.sh
+./xrdp.sh
+
+echo "################################################################"
+echo "################ All Pacahges Has been Installed ######################"
+echo "################################################################"
+
