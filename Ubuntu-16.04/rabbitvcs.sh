2e6de4294ea5a098c5de4b524ba934bcaa2ecd0e
Vinod Kumar <vinod.kumar@impressico.com>
2017-11-16 06:22:00 -0500

    rabbitvcs

---
+++ Ubuntu-16.04/rabbitvcs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+########################################################
+# Written to be used on 64 bits computers
+# Written By    :       Vinod Kumar
+########################################################
+########################################################
+########################################################
+
+# Downloading & Installing Skype for Ubuntu Version
+sudo add-apt-repository ppa:rabbitvcs/ppa
+sudo apt-get update -y
+sudo apt-get install rabbitvcs* -y 
+echo "################################################################"
+echo "################ Installation Sucessfully ######################"
+echo "################################################################"
