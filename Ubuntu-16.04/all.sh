835d6c3751762998a022797c21ad586d494bf67d
Vinod Kumar <vinod.kumar@impressico.com>
2017-11-24 08:06:17 -0500

    Add changes

--- Ubuntu-16.04/all.sh
+++ Ubuntu-16.04/all.sh
@@ -12,22 +12,21 @@ sudo ./adobereader.sh
 sudo ./beyond-compare.sh
 sudo ./chrome.sh
 sudo ./eclipse-neon.sh
+sudo ./java-1.7.sh
+sudo ./maven-3.3.9.sh
 sudo ./notepadqq.sh
 sudo ./openssh.sh
 sudo ./owncloud.sh
+sudo ./rabbitvcs.sh
 sudo ./remmina.sh
 sudo ./shutter.sh
-sudo ./skype.sh
 sudo ./slack.sh
 sudo ./sublime.sh
+sudo ./teamviewer.sh
 sudo ./vlc.sh
 sudo ./xrdp.sh
-sudo ./java-1.7.sh
-sudo ./teamviewer.sh
-sudo ./maven-3.3.9.sh
-sudo ./rabbitvcs.sh
-sudo ./intelliJ.sh
 sudo ./java-1.8.sh
+sudo ./intelliJ.sh
 echo "################################################################"
 echo "################ All Packages Has been Installed ######################"
 echo "################################################################"
