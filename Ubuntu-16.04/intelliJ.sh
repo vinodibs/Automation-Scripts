31e94d6eb8c6db53c9d91456f63e69add506bb21
vinodibs <32012530+vinodibs@users.noreply.github.com>
2017-12-19 21:51:24 +0530

    IntelliJ

--- Ubuntu-16.04/intelliJ.sh
+++ Ubuntu-16.04/intelliJ.sh
@@ -3,17 +3,11 @@
 # Written to be used on 64 bits computers
 # Written By    :       Vinod Kumar
 ########################################################
-########################################################
-########################################################
-
-# Downloading & Installing elipse for Ubuntu Version
-
+# Downloading & Installing intelliJ for Ubuntu Version
 wget http://git.impressicocrm.com/vinod.kumar/Software/raw/master/ideaIU-2017.2.5.tar.gz -O /tmp/ideaIU-2017.2.5.tar.gz
 cd /tmp 
 sudo tar -xvf ideaIU-2017.2.5.tar.gz -C ~/Documents/
 cd ~/Documents/idea-IU-172.4343.14/bin/
 sudo ./idea.sh
-echo "################################################################"
-echo "################ Installation Sucessfully ######################"
-echo "################################################################"
+echo "################ IntelliJ Installation Sucessfully ######################"
 
