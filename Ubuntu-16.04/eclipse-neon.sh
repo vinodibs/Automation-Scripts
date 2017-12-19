69cdeb83fff64d80b74208c999a5e187d82c5ff1
vinodibs <32012530+vinodibs@users.noreply.github.com>
2017-12-19 21:44:53 +0530

    Eclipse Neon

--- Ubuntu-16.04/eclipse-neon.sh
+++ Ubuntu-16.04/eclipse-neon.sh
@@ -3,15 +3,9 @@
 # Written to be used on 64 bits computers
 # Written By    :       Vinod Kumar
 ########################################################
-########################################################
-########################################################
-
 # Downloading & Installing elipse for Ubuntu Version
-
 wget http://git.impressicocrm.com/vinod.kumar/Automation-Scritps/raw/master/eclipse-jee-neon-R-linux-gtk-x86_64.tar.gz -O /tmp/eclipse-jee-neon-R-linux-gtk-x86_64.tar.gz
 cd /tmp 
 sudo tar -xvf eclipse-jee-neon-R-linux-gtk-x86_64.tar.gz -C ~/Desktop/
-echo "################################################################"
-echo "################ Installation Sucessfully ######################"
-echo "################################################################"
+echo "################ Eclipse Neon Installation Sucessfully #####################"
 
