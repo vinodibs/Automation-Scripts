f0d4c9310cc83f67caac42762ef9f582ee941f2a
vinodibs <32012530+vinodibs@users.noreply.github.com>
2017-12-19 22:07:36 +0530

    Update Package

--- Ubuntu-16.04/package.sh
+++ Ubuntu-16.04/package.sh
@@ -7,13 +7,14 @@
 ########################################################
 
 # Installing Required Package for Ubuntu Version
-sudo apt-get update -y 
-sudo apt-get install net-tools -y 
+sudo apt-get update -y
+sudo apt-get install openssh-server -y
+sudo apt-get install net-tools -y
 sudo apt-get install network-manager* -y
 sudo apt-get install vim -y
 sudo apt-get -f install -y
 sudo apt-get install alsa-base pulseaudio -y
-sudo add-apt-repository ppa:git-core/ppa 
+sudo add-apt-repository ppa:git-core/ppa
 sudo apt update; sudo apt install git -y
 sudo alsa force-reload
 mkdir -p ~/Documents/Source-Code
@@ -22,6 +23,4 @@ sudo echo "192.168.0.11       "impressico.local" " >> /etc/hosts
 sudo echo "192.168.0.68       "hrms.impressico.local" " >> /etc/hosts
 sudo echo "192.168.0.21       "svn.impressico.in" " >> /etc/hosts
 sudo echo "192.168.0.7       "chef.impressico.local" " >> /etc/hosts
-echo "################################################################"
-echo "################ Installation Sucessfully ######################"
-echo "################################################################"
+echo "################ openssh-vim-gitupdate-alsa Installation Sucessfully ######################"
