249ae110df194e3a2d429d3b5ca92a95fa68b224
vinodibs <32012530+vinodibs@users.noreply.github.com>
2017-12-19 22:02:13 +0530

    Own cloud

--- Ubuntu-16.04/owncloud.sh
+++ Ubuntu-16.04/owncloud.sh
@@ -3,13 +3,9 @@
 # Written to be used on 64 bits computers
 # Written By    :       Vinod Kumar
 ########################################################
-########################################################
-########################################################
 # Downloading & Installing Owncloud Client for Ubuntu Version
 sh -c "echo 'deb http://download.opensuse.org/repositories/isv:/ownCloud:/desktop/xUbuntu_15.04/ /' &gt;&gt; /etc/apt/sources.list.d/owncloud-client.list" 
-sudo apt-get update -y 
-sudo apt-get install owncloud-client -y 
-echo "################################################################"
-echo "################ Installation Sucessfully ######################"
-echo "################################################################"
+sudo apt-get update
+sudo apt-get install owncloud-client -y
+echo "################ Own cloud client Installation Sucessfully ######################"
 
