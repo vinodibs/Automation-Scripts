7d125af46018f3b67947d77afca5e9bd437a627c
vinodibs <32012530+vinodibs@users.noreply.github.com>
2017-12-19 22:00:01 +0530

    Notepad++

--- Ubuntu-16.04/notepadqq.sh
+++ Ubuntu-16.04/notepadqq.sh
@@ -3,15 +3,9 @@
 # Written to be used on 64 bits computers
 # Written By    :       Vinod Kumar
 ########################################################
-########################################################
-########################################################
-
-# Downloading & Installing Skype for Ubuntu Version
-
-sudo add-apt-repository ppa:notepadqq-team/notepadqq -y
-sudo apt-get update -y
+# Downloading & Installing Notepad ++ for Ubuntu Version
+sudo add-apt-repository ppa:notepadqq-team/notepadqq
+sudo apt-get update
 sudo apt-get install notepadqq -y
 echo "################################################################"
-echo "################ Installation Sucessfully ######################"
-echo "################################################################"
-
+echo "################ Notepad++ Installation Sucessfully ######################"
