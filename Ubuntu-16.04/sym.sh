88a89656cde7fca8e8faecc197290776a1e5f3e3
Vinod Kumar <vinod.kumar@impressico.com>
2017-12-19 09:00:11 -0500

    sy

--- Ubuntu-16.04/sym.sh
+++ Ubuntu-16.04/sym.sh
@@ -1,1 +1,7 @@
 http://mail:8014/EmailInstallPackages/2BA71E85C0A8000B0177137E5570A429_LINUXDPKG/sep/SymantecEndpointProtection.zip
+
+
+
+sudo apt-get install oracle-java8-unlimited-jce-policy
+sudo apt-get install libc6:i386 libx11-6:i386 libncurses5:i386 libstdc++6:i386 -y 
+sudo apt-get install lib32ncurses5 lib32z1 -y
