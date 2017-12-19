d24b5c4d08b97e282d577aace7552aa9383a0740
vinodibs <32012530+vinodibs@users.noreply.github.com>
2017-12-19 21:47:36 +0530

    Gargle Install

--- Ubuntu-16.04/gradle-2.sh
+++ Ubuntu-16.04/gradle-2.sh
@@ -3,14 +3,8 @@
 # Written to be used on 64 bits computers
 # Written By    :       Vinod Kumar
 ########################################################
-########################################################
-########################################################
-
 # Downloading & Installing Gradle-2  for Ubuntu Version
-
 sudo add-apt-repository ppa:cwchien/gradle
-sudo apt-get update -y 
+sudo apt-get update
 sudo apt-get install gradle -y 
-echo "################################################################"
-echo "################ Installation Sucessfully ######################"
-echo "################################################################"
+echo "################ Gardle Installation Sucessfully ######################"
