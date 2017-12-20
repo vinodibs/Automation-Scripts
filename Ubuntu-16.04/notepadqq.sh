4512d0976c5c58fcaf866952ea27dce2798ee2d1
vinodibs <32012530+vinodibs@users.noreply.github.com>
2017-12-20 16:34:34 +0530

    Notepad ++

--- Ubuntu-16.04/notepadqq.sh
+++ Ubuntu-16.04/notepadqq.sh
@@ -4,8 +4,8 @@
 # Written By    :       Vinod Kumar
 ########################################################
 # Downloading & Installing Notepad ++ for Ubuntu Version
-sudo add-apt-repository ppa:notepadqq-team/notepadqq
-sudo apt-get update
+sudo add-apt-repository ppa:notepadqq-team/notepadqq -y 
+sudo apt-get update -y 
 sudo apt-get install notepadqq -y
 echo "################################################################"
 echo "################ Notepad++ Installation Sucessfully ######################"
