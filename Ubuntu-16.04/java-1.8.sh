5390d37f9c97c3cbcf550042fd7ec8be11ce11f6
Vinod Kumar <vinod.kumar@impressico.com>
2017-12-21 01:55:09 +0530

    Java 1.8

--- Ubuntu-16.04/java-1.8.sh
+++ Ubuntu-16.04/java-1.8.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+########################################################
+# Written to be used on 64 bits computers
+# Written By    :       Vinod Kumar
+######################################################
+# Downloading & Installing Java-1.8 for Ubuntu Version
 sudo add-apt-repository ppa:webupd8team/java -y
 sudo apt-get update -y
 sudo apt -f install -y
 sudo accept-license = "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true"
 sudo echo "$accept-license" | sudo debconf-set-selections
 sudo apt --assume-yes install oracle-java8-installer
+sudo apt-get install oracle-java8-unlimited-jce-policy -y
+echo "################ Java 1.8 Installation Sucessfully ######################"
