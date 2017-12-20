0425e391ebad19090e76c5ee195896cc02ce78d6
Vinod Kumar <vinod.kumar@impressico.com>
2017-12-21 01:49:48 +0530

    Comodo Antivirus

--- Ubuntu-16.04/comodo.sh
+++ Ubuntu-16.04/comodo.sh
@@ -4,12 +4,10 @@
 # Written By    :       Vinod Kumar
 ########################################################
 # Downloading & Installing COMODO ANTIVIRUS for Ubuntu Version
-sudo bash
-cd /tmp/
-wget http://cdn.download.comodo.com/cis/download/installs/linux/cav-linux_x64.deb#_ga=2.135382835.308718117.1513683863-1805346075.1513683863
-wget http://security.ubuntu.com/ubuntu/pool/universe/o/openssl098/libssl0.9.8_0.9.8o-7ubuntu3.2.14.04.1_amd64.deb
-sudo dpkg -i libssl0.9.8_0.9.8o-7ubuntu3.2.14.04.1_amd64.deb
+wget http://cdn.download.comodo.com/cis/download/installs/linux/cav-linux_x64.deb#_ga=2.135382835.308718117.1513683863-1805346075.1513683863 -O /tmp/cav-linux_x64.deb
+wget http://security.ubuntu.com/ubuntu/pool/universe/o/openssl098/libssl0.9.8_0.9.8o-7ubuntu3.2.14.04.1_amd64.deb -O /tmp/libssl0.9.8_0.9.8o-7ubuntu3.2.14.04.1_amd64.deb
+sudo dpkg -i /tmp/libssl0.9.8_0.9.8o-7ubuntu3.2.14.04.1_amd64.deb
 sudo apt-get install -f
-sudo dpkg -i cav-linux_x64.deb
+sudo dpkg -i /tmp/cav-linux_x64.deb
 sudo /opt/COMODO/post_setup.sh
 echo "################ Comodo Installation Sucessfully ######################"
