20446189d7d26a5a5e284b8d842f42b524347263
vinodibs <32012530+vinodibs@users.noreply.github.com>
2018-01-10 14:13:13 +0530

    Update shutter.sh

--- Ubuntu-16.04/shutter.sh
+++ Ubuntu-16.04/shutter.sh
@@ -3,13 +3,8 @@
 # Written to be used on 64 bits computers
 # Written By    :       Vinod Kumar
 ########################################################
-########################################################
-########################################################
-
 # Downloading & Installing Shutter for Ubuntu Version
 sudo add-apt-repository ppa:shutter/ppa -y 
 sudo apt-get update -y 
 sudo apt-get install shutter -y 
-echo "################################################################"
-echo "################ Installation Sucessfully ######################"
-echo "################################################################"
+echo "################ Shutter Installation Sucessfully ######################"
