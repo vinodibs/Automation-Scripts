21cfeccd2dacea1e298125d253193d0daddc088a
vinodibs <32012530+vinodibs@users.noreply.github.com>
2018-01-10 14:20:00 +0530

    Update xrdp.sh

--- Ubuntu-16.04/xrdp.sh
+++ Ubuntu-16.04/xrdp.sh
@@ -3,16 +3,10 @@
 # Written to be used on 64 bits computers
 # Written By    :       Vinod Kumar
 ########################################################
-########################################################
-########################################################
-
 # Installing & Downloading Xrdp for Ubuntu Version
 sudo apt-get update -y 
 sudo apt-get install xrdp -y 
 sudo apt-get install xfce4 -y 
 sudo echo xfce4-session >~/.xsession
 sudo service xrdp restart
-
-echo "################################################################"
-echo "################ Installation Sucessfully ######################"
-echo "################################################################"
+echo "################ XRDP Installation Sucessfully ######################"
