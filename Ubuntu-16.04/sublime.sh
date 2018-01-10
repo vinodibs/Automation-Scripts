27e82c44261a30929d14ec46d701399bdf072a69
vinodibs <32012530+vinodibs@users.noreply.github.com>
2018-01-10 14:15:30 +0530

    Update sublime.sh

--- Ubuntu-16.04/sublime.sh
+++ Ubuntu-16.04/sublime.sh
@@ -3,9 +3,6 @@
 # Written to be used on 64 bits computers
 # Written By    :       Vinod Kumar
 ########################################################
-########################################################
-########################################################
-
 # Downloading & Installing Sublime Text for Ubuntu Version
 sudo wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
