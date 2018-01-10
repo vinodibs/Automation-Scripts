622ac4d1c8a16b850217b833ce327317f0e6ebb6
vinodibs <32012530+vinodibs@users.noreply.github.com>
2018-01-10 13:55:49 +0530

    Update chrome.sh

--- Ubuntu-16.04/chrome.sh
+++ Ubuntu-16.04/chrome.sh
@@ -4,7 +4,7 @@
 # Written By    :       Vinod Kumar
 ######################################################
 # Downloading & Installing Chrome for Ubuntu Version
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O /tmp/google-chrome-stable_current_amd64.deb
+sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O /tmp/google-chrome-stable_current_amd64.deb
 sudo dpkg -i /tmp/google-chrome-stable_current_amd64.deb
 echo "################ Chrome Installation Sucessfully ######################"
 
