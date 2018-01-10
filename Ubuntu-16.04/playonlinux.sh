0cbebbf779e9c7b089927c71672c573559054239
vinodibs <32012530+vinodibs@users.noreply.github.com>
2018-01-10 16:12:17 +0530

    Update playonlinux.sh

--- Ubuntu-16.04/playonlinux.sh
+++ Ubuntu-16.04/playonlinux.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+########################################################
+# Written to be used on 64 bits computers
+# Written By    :       Vinod Kumar
+########################################################
 sudo add-apt-repository ppa:noobslab/apps
 sudo apt-get update
 sudo apt-get install playonlinux -y
+echo "################ Playonlinux Installation Sucessfully ######################"
