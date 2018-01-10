7c86fa0edfae14596be5cee65645577167028d32
vinodibs <32012530+vinodibs@users.noreply.github.com>
2018-01-10 14:06:16 +0530

    Update comodo.sh

--- Ubuntu-16.04/comodo.sh
+++ Ubuntu-16.04/comodo.sh
@@ -7,7 +7,7 @@
 wget http://cdn.download.comodo.com/cis/download/installs/linux/cav-linux_x64.deb#_ga=2.135382835.308718117.1513683863-1805346075.1513683863 -O /tmp/cav-linux_x64.deb
 wget http://security.ubuntu.com/ubuntu/pool/universe/o/openssl098/libssl0.9.8_0.9.8o-7ubuntu3.2.14.04.1_amd64.deb -O /tmp/libssl0.9.8_0.9.8o-7ubuntu3.2.14.04.1_amd64.deb
 sudo dpkg -i /tmp/libssl0.9.8_0.9.8o-7ubuntu3.2.14.04.1_amd64.deb
-sudo apt-get install -f
+sudo apt-get -f install -y
 sudo dpkg -i /tmp/cav-linux_x64.deb
 sudo /opt/COMODO/post_setup.sh
 echo "################ Comodo Installation Sucessfully ######################"
