a0891791d16e64ff9780e52db9e9b5ca0fcede4d
vinodibs <32012530+vinodibs@users.noreply.github.com>
2018-01-10 14:13:56 +0530

    Update skype.sh

--- Ubuntu-16.04/skype.sh
+++ Ubuntu-16.04/skype.sh
@@ -3,14 +3,10 @@
 # Written to be used on 64 bits computers
 # Written By 	: 	Vinod Kumar
 ########################################################
-########################################################
-########################################################
 # Downloading & Installing Skype for Ubuntu Version
 rm /tmp/skypeforlinux-64.deb
 wget https://repo.skype.com/latest/skypeforlinux-64.deb -O /tmp/skypeforlinux-64.deb
 sudo apt install -y libjpeg62:i386
 sudo dpkg -i /tmp/skypeforlinux-64.deb
+echo "################ Skype for linux Installation Sucessfully ######################"
 
-echo "################################################################"
-echo "################ Installation Sucessfully ######################"
-echo "################################################################"
