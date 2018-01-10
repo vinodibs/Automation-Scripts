164d1219517ed696c24540a796988169a38ecb6e
vinodibs <32012530+vinodibs@users.noreply.github.com>
2018-01-10 14:15:04 +0530

    Update slack.sh

--- Ubuntu-16.04/slack.sh
+++ Ubuntu-16.04/slack.sh
@@ -4,23 +4,12 @@
 # Written to be used on 64 bits computers
 # Written By 	: 	Vinod Kumar
 ###############################################
-###############################################
-#
-#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
-#
-###############################################
-
 rm /tmp/slack-desktop-2.8.2-amd64.deb
-wget http://downloads.slack-edge.com/linux_releases/slack-desktop-2.8.2-amd64.deb -O /tmp/slack-desktop-2.8.2-amd64.deb
+sudo wget http://downloads.slack-edge.com/linux_releases/slack-desktop-2.8.2-amd64.deb -O /tmp/slack-desktop-2.8.2-amd64.deb
 sudo dpkg -i /tmp/slack-desktop-2.8.2-amd64.deb
-
 echo "fixing hardcoded icon"
 old="Icon=\/usr\/share\/pixmaps\/slack.png"
 new="Icon=slack"
 location="/usr/share/applications/slack.desktop"
 sudo sed -i s/$old/$new/g $location
-
-
-echo "################################################################"
-echo "###################    T H E   E N D      ######################"
-echo "################################################################"
+echo "##################### Slack Desktop Install Sucessfully ########################"
