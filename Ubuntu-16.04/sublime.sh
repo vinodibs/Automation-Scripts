e3e4cc88b759e8229f00d3dc81fdcbcb9d84b9f6
vinodibs <32012530+vinodibs@users.noreply.github.com>
2018-01-10 14:00:56 +0530

    Update sublime.sh

--- Ubuntu-16.04/sublime.sh
+++ Ubuntu-16.04/sublime.sh
@@ -10,7 +10,7 @@
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update -y 
+sudo apt-get -f install
 sudo apt-get install sublime-text -y 
-echo "################################################################"
-echo "################ Installation Sucessfully ######################"
-echo "################################################################"
+echo "################ Sublime Install Sucessfully ######################"
+
