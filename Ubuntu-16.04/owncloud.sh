549ea62e79dc9cbb924d9c725b9808084c14d6c3
vinodibs <32012530+vinodibs@users.noreply.github.com>
2018-01-10 13:41:04 +0530

    Update owncloud.sh

--- Ubuntu-16.04/owncloud.sh
+++ Ubuntu-16.04/owncloud.sh
@@ -6,6 +6,7 @@
 # Downloading & Installing Owncloud Client for Ubuntu Version
 sudo sh -c "echo 'deb http://download.opensuse.org/repositories/isv:/ownCloud:/desktop/Ubuntu_16.04/ /' > /etc/apt/sources.list.d/owncloud-client.list"
 sudo apt-get update
+sudo apt-get -f install
 sudo apt-get install owncloud-client -y
 echo "################ Own cloud client Installation Sucessfully ######################"
 
