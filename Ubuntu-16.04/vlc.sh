0bbf1af6701a82b3b06d9be5b76d714851d7b9e4
vinodibs <32012530+vinodibs@users.noreply.github.com>
2018-01-10 14:17:19 +0530

    Update vlc.sh

--- Ubuntu-16.04/vlc.sh
+++ Ubuntu-16.04/vlc.sh
@@ -3,13 +3,9 @@
 # Written to be used on 64 bits computers
 # Written By    :       Vinod Kumar
 ########################################################
-########################################################
-########################################################
-
 # Downloading & Installing VLC for Ubuntu Version
 sudo add-apt-repository ppa:videolan/master-daily -y
 sudo apt-get update -y 
 sudo apt-get install vlc -y
-echo "################################################################"
-echo "################ Installation Sucessfully ######################"
-echo "################################################################"
+echo "################ VLC Installation Sucessfully ######################"
+
