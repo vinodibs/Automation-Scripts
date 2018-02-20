dc32663fa1ff6c0787d5c4e4a57508c16d291466
vinodibs <32012530+vinodibs@users.noreply.github.com>
2018-02-20 17:13:39 +0530

    Update kazam.sh

--- Ubuntu-16.04/kazam.sh
+++ Ubuntu-16.04/kazam.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+########################################################
+# Written to be used on 64 bits computers
+# Written By    :       Vinod Kumar
+#######################################################
+# Installing All required Package for Ubuntu Version
 sudo apt-add-repository ppa:sylvain-pineau/kazam
 sudo apt-get update
 sudo apt-get install kazam 
