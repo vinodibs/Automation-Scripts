2475f0922532ae4318f9148d96af1bf0f66d392e
vinodibs <32012530+vinodibs@users.noreply.github.com>
2018-02-20 17:14:24 +0530

    Update kazam.sh

--- Ubuntu-16.04/kazam.sh
+++ Ubuntu-16.04/kazam.sh
@@ -4,6 +4,7 @@
 # Written By    :       Vinod Kumar
 #######################################################
 # Installing All required Package for Ubuntu Version
-sudo apt-add-repository ppa:sylvain-pineau/kazam
+sudo apt-add-repository ppa:sylvain-pineau/kazam -y
 sudo apt-get update
-sudo apt-get install kazam 
+sudo apt-get install kazam -y
+echo "################ Maven Installation Sucessfully ######################"
