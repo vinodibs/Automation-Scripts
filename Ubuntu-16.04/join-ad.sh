df686f86c88dd710f3b390f68a30de9f3a2d4f0d
vinodibs <32012530+vinodibs@users.noreply.github.com>
2018-03-14 11:57:24 +0530

    Update join-ad.sh

--- Ubuntu-16.04/join-ad.sh
+++ Ubuntu-16.04/join-ad.sh
@@ -6,3 +6,9 @@
 # Downloading & Installing join-ad for Ubuntu Version
 sudo sed -i '12 s/^/#/' /etc/nsswitch.conf
 sudo echo "hosts:       "files " "dns"  ""mdns4" >> /etc/nsswitch.conf
+service networking restart
+wget http://git.impressicocrm.com/vinod.kumar/Software/raw/master/centrify-suite-2017.3-deb7-x86_64.tgz
+sudo tar -zvxf centrify-suite-2017.3-deb7-x86_64.tgz
+cd centrify-suite-2017.3-deb7-x86_64
+sudo sh install.sh
+
