8bdefbf725976a86df00efb267f6f94864c6d70d
vinodibs <32012530+vinodibs@users.noreply.github.com>
2018-03-24 21:35:39 +0530

    Update join-ad.sh

--- Ubuntu-16.04/join-ad.sh
+++ Ubuntu-16.04/join-ad.sh
@@ -7,8 +7,8 @@
 sudo sed -i '12 s/^/#/' /etc/nsswitch.conf
 sudo echo "hosts:       "files " "dns"  ""mdns4" >> /etc/nsswitch.conf
 service networking restart
-sudo wget http://git.impressicocrm.com/vinod.kumar/Software/raw/master/centrify-suite-2017.3-deb7-x86_64.tgz -O /tmp/centrify-suite-2017.3-deb7-x86_64.tgz
-sudo mkdir /tmp/centrify && cd /tmp/centrify
-sudo tar -zvxf /tmp/centrify-suite-2017.3-deb7-x86_64.tgz -C /tmp/centrify
+wget http://git.impressicocrm.com/vinod.kumar/Software/raw/master/centrify-suite-2017.3-deb7-x86_64.tgz -O /tmp/centrify-suite-2017.3-deb7-x86_64.tgz
+mkdir /tmp/centrify && cd /tmp/centrify
+tar -zvxf /tmp/centrify-suite-2017.3-deb7-x86_64.tgz -C /tmp/centrify
 sudo sh install.sh
 echo "################ join-ad Installation Sucessfully ######################"
