84c2953315bfb3783c867f64a68b5feaeca19050
vinodibs <32012530+vinodibs@users.noreply.github.com>
2018-05-12 16:12:34 +0530

    Update join-ad-ubuntu.sh
    initial commit

--- Ubuntu-16.04/join-ad-ubuntu.sh
+++ Ubuntu-16.04/join-ad-ubuntu.sh
@@ -10,5 +10,23 @@ service networking restart
 wget http://git.impressicocrm.com/vinod.kumar/Software/raw/master/centrify-suite-2017.3-deb7-x86_64.tgz -O /tmp/centrify-suite-2017.3-deb7-x86_64.tgz
 mkdir /tmp/centrify && cd /tmp/centrify
 tar -zvxf /tmp/centrify-suite-2017.3-deb7-x86_64.tgz -C /tmp/centrify
-sudo sh install.sh
-echo "################ join-ad Installation Sucessfully ######################"
+cd /tmp/centrify
+sudo sed -i '3 s/^/#/' centrify-suite.cfg 
+sudo sed -i '7 s/^/#/' centrify-suite.cfg
+sudo sed -i '11,13 s/^/#/' centrify-suite.cfg
+sudo sed -i '20 s/^/#/' centrify-suite.cfg
+sed -i '3i ADCHECK="Y"' centrify-suite.cfg
+sed -i '8i ADJOIN="Y"' centrify-suite.cfg
+sed -i '13i DOMAIN="impressico.local"' centrify-suite.cfg
+sed -i '14i USERID=skype' centrify-suite.cfg
+sed -i '15i PASSWD="noida@123"' centrify-suite.cfg
+sed -i '25i REBOOT="Y"' centrify-suite.cfg
+echo "ADCHECK=Y" >> centrify-suite.cfg
+echo "ADJOIN=N" >> centrify-suite.cfg
+echo "DOMAIN=impressico.local" >> centrify-suite.cfg
+echo "USERID=skype" >> centrify-suite.cfg
+echo "PASSWD=noida@123" >> centrify-suite.cfg
+echo "REBOOT=Y" >> centrify-suite.cfg
+sudo sh install.sh --express
+echo"Ad-has been joined sucessfully & system will reboot automatically "
+
