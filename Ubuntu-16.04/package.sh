dc6842fab6c343f48305f5379418e7ad14602b08
vinodibs <32012530+vinodibs@users.noreply.github.com>
2018-05-24 13:50:25 +0530

    Update package.sh

--- Ubuntu-16.04/package.sh
+++ Ubuntu-16.04/package.sh
@@ -23,7 +23,7 @@ sudo alsa force-reload
 mkdir -p ~/Documents/Source-Code
 sudo apt-get install libc6:i386 libx11-6:i386 libncurses5:i386 libstdc++6:i386 -y
 sudo apt-get install lib32ncurses5 lib32z1 -y
-sed -i '35i :"%admin-it  ALL=LALL:ALL) ALL"' /etc/sudoers
+sed -i '35i "%admin-it  ALL=LALL:ALL) ALL"' /etc/sudoers
 #sudo echo "192.168.0.1       "icloud.impressico.local" " >> /etc/hosts
 #sudo echo "192.168.0.11       "impressico.local" " >> /etc/hosts
 #sudo echo "192.168.0.68       "hrms.impressico.local" " >> /etc/hosts
